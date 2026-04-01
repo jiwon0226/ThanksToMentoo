c9711d216861f0a9f8e5e2b9b3a75827fddef3fa
jiwon0226 <ppg0226@naver.com>
2026-04-01 16:06:02 +0900

    add

--- Day9_project/navigate.js
+++
@@ -1,7 +0,0 @@
-function openNav() {
-    document.getElementById("sidebar").style.width = "200px";
-}
-
-function closeNav() {
-    document.getElementById("sidebar").style.width = "0";
-}
--- README.md
+++
@@ -1,2 +0,0 @@
-# Resat_Challenge_11
-연결 테스트2 확인
